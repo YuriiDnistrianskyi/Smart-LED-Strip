97cd729b4c6888282ae02a282a2d259d91292112
YuraDnistriansky <yura.dniester@gmail.com>
2025-05-06 18:13:02 +0300

    fix wifi info

--- ESP8266/Main/espServer/infoWiFi.h
+++ ESP8266/Main/espServer/infoWiFi.h
@@ -1,7 +1,7 @@
 #ifndef INFOWIFI_H
 #define INFOWIFI_H
 
-const char* ssid = "DickTown";
-const char* password = "45848728";
+const char* ssid = "WiFi";
+const char* password = "**********";
 
 #endif // INFOWIFI_H
