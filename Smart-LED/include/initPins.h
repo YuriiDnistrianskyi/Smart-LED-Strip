9df4e76dd8c7d824777b66b2423266e3c7887794
YuraDnistriansky <yura.dniester@gmail.com>
2025-06-09 22:00:54 +0300

    mini fix

--- Smart-LED/include/initPins.h
+++ Smart-LED/include/initPins.h
@@ -1,10 +1,6 @@
 #ifndef INITPINS_H
 #define INITPINS_H
 
-#include <Arduino.h>
-
-void setButtonState();
-void IRAM_ATTR handleButton();
 void initPins();
 
 #endif // INITPINS_H
--- Smart-LED/include/initServer.h
+++ Smart-LED/include/initServer.h
@@ -1,6 +1,11 @@
 #ifndef INIT_SERVER_H
 #define INIT_SERVER_H
 
+#include <ESPAsyncWebServer.h>
+
+void setGoLighting();
+void notifyClients();
+void handleWebSocket(AsyncWebSocket* ws, AsyncWebSocketClient* client, AwsEventType type, void* arg, uint8_t* data, size_t len);
 void initServer();
 
 #endif // INIT_SERVER_H
